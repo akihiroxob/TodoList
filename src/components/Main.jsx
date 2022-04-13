19d6570a252029d371bbd7f5eadf193edf8eec58
akihiro okayama <akihiro.ob@gmail.com>
2022-04-13 11:48:31 +0900

    adjust window size

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,15 @@ import React, {useEffect, useRef} from 'react';
 import {ignoreMouse, setSize} from '../libs/IPC';
 import TodoList from './TodoList.jsx';
 
+const BODY_MARGIN = 16;
 export default (props) => {
     const mainElement = useRef(null);
 
     useEffect(() => {
         if (!mainElement.current) return;
         const height = mainElement.current.clientHeight;
-        const width = props.data.length * 240 + (props.data.length - 1) * 8;
-        setSize(height, width);
+        const width = props.data.length * 240 + (props.data.length - 1) * 8 + BODY_MARGIN;
+        setSize(height, wi);
     }, [props.data]); // <-- here put the parameter to listen
 
     return (
